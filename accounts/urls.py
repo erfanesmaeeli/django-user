370b01870fb87f3f954b77da13df2f8bf8f9a6c7
erfan esmaeeli <bestwork.erfan@gmail.com>
2021-06-06 20:10:25 +0430

    create LoginView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
+from .views import LoginView
 app_name = 'accounts'
 
 urlpatterns = [
-	# path('login/', ..., name='login'),
+	path('login/', LoginView.as_view(), name='login'),
 	# path('register/', ..., name='login'),
 	# path('logout/', ..., name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import User
+from django.contrib.auth.views import LoginView as BaseLoginView
 
-# Create your views here.
+
+class LoginView(BaseLoginView):
+    template_name = 'accounts/login.html'
+    success_url = "/"
+
+    def get_success_url(self):
+        return str(self.success_url)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            #TODO This line of code is not working properly !
+            request.session["next"] = request.GET['next']
+            self.success_url = request.session["next"]
+        except KeyError:
+            pass            
+        return self.render_to_response(self.get_context_data())
+
+    
+    
+
